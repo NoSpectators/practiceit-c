91fc1b9c67c642fd7b76e45e13436a0cbc650209
NoSpectators <john.c.erickson@gmail.com>
2026-03-24 20:32:19 -0400

    works

---
+++ ch1/egg.c
@@ -0,0 +1,23 @@
+/* write a complete program that displays the following output:
+    _______
+   /       \ 
+  /         \ 
+  -"-'-"-'-"-
+  \         / 
+   \_______/
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("   _______\n");
+    printf("  /       \\\n");
+    printf(" /         \\\n");
+    printf(" -\"-'-\"-'-\"-\n");
+    printf(" \\         /\n");
+    printf("  \\_______/\n");
+
+    return 0;
+}
+
