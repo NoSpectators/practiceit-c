aaba7021033029099ce188d180ce830026690f32
NoSpectators <john.c.erickson@gmail.com>
2026-03-31 08:09:37 -0400

    works

---
+++ ch1/shining.c
@@ -0,0 +1,10 @@
+/* Write a program in a class named Shining that prints the following line of 
+ output 1000 times: All work and no play makes Jack a dull boy. */
+#include <stdio.h>
+
+
+int main() {
+    for (int i = 1; i <= 1000; i++) {
+        printf("All work and no play makes Jack a dull boy.\n");
+    } 
+}
