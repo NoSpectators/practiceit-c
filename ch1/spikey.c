febe2358f75f5bfa9928b548d7b278c4ca5b1de3
NoSpectators <john.c.erickson@gmail.com>
2026-04-01 10:14:50 -0400

    works

---
+++ ch1/spikey.c
@@ -0,0 +1,20 @@
+/* Write a complete program in a class named Spikey that prints the 
+ following output:
+   \/
+  \\//
+ \\\///
+ ///\\\
+  //\\
+   /\
+*/
+
+#include <stdio.h>
+int main() {
+    printf("  \\/  \n");
+    printf(" \\\\// \n");
+    printf("\\\\\\///\n");
+    printf("///\\\\\\\n"); 
+    printf(" //\\\\\n");
+    printf("  /\\  \n");
+    return 0;
+}
