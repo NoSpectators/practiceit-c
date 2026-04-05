72005c36d6bc6e9813107693c01a916709710905
NoSpectators <john.c.erickson@gmail.com>
2026-04-04 22:23:39 -0400

    it works

---
+++ ch1/stewie.c
@@ -0,0 +1,15 @@
+/* Write a complete Java program in a class named Stewie that prints the
+ following output:
+ //////////////////////
+ || Victory is mine! ||
+ \\\\\\\\\\\\\\\\\\\\\\
+*/
+
+#include <stdio.h>
+
+
+int main() {
+    printf("//////////////////////\n");
+    printf("|| Victory is mine! ||\n");
+    printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+}
