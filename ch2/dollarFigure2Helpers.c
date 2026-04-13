7ddd9789e4d31bd7783d0392eed217fc5bdbff9a
NoSpectators <john.c.erickson@gmail.com>
2026-04-13 09:18:30 -0400

    refactored with helper functions

---
+++ ch2/dollarFigure2Helpers.c
@@ -0,0 +1,48 @@
+/* Write a Fortran program called DollarFigure2 that uses a global constant
+ for the figure's height
+
+ $$$$$$$**************$$$$$$$
+ **$$$$$$************$$$$$$**
+ ****$$$$$**********$$$$$****
+ ******$$$$********$$$$******
+ ********$$$******$$$********
+ **********$$****$$**********
+ ************$**$************
+ 
+*/
+
+#include <stdio.h>
+
+void outerStars(int i);
+void dollarSigns(int i, int numRows);
+void middleStars(int i, int numRows);
+
+int main() { 
+    int i, j, k, l, m, n;
+    int numRows = 10; // arbitrary height
+   
+    // outer loop based on height
+    for (int i = 0; i < numRows; i++) {  
+        outerStars(i); 
+        dollarSigns(i, numRows);
+        middleStars(i, numRows);
+        dollarSigns(i, numRows);
+        outerStars(i); 
+        printf("\n"); 
+    }
+}
+void outerStars(int i) {
+    for (int j = 0; j < 2 * i; j++) {
+        printf("*");
+    }
+}
+void dollarSigns(int i, int numRows) {
+    for (int k = 0; k < numRows - i; k++) {   
+        printf("$");
+    } 
+}
+void middleStars(int i, int numRows) {
+    for (int l = 0; l < 2 * numRows - i * 2; l++) {
+        printf("*");
+    } 
+}
