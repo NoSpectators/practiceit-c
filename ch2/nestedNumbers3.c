a8ff08c8b9de6fb1375ac795bee18ba68e7b2fe3
NoSpectators <john.c.erickson@gmail.com>
2026-04-16 20:30:32 -0400

    works

---
+++ ch2/nestedNumbers3.c
@@ -0,0 +1,20 @@
+/* Modify your code from the previous problem to produce the following output:
+
+ 999999999888888887777777666666555554444333221
+ 999999999888888887777777666666555554444333221
+ 999999999888888887777777666666555554444333221
+ 999999999888888887777777666666555554444333221
+*/
+
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 9; j >= 1; j--) { 
+            for (int k = j; k >= 1; k--) {
+                printf("%d", j);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
