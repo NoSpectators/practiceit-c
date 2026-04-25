e797c213a84c2d0dac836305d1818e452f316565
NoSpectators <john.c.erickson@gmail.com>
2026-04-24 21:15:18 -0400

    works

---
+++ ch2/spacesAndNumbers.c
@@ -0,0 +1,22 @@
+/* Write nested DO loops to produce the following output:
+     1
+    22
+   333
+  4444
+ 55555
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 5; j >= i; j--) {
+            printf(" "); 
+        }
+        for (int k = 1; k <= i; k++) {
+            printf("%d", i);
+	}
+        printf("\n");
+    }
+    return 0;
+}
