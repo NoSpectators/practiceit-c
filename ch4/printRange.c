c4f6eabd7e901f87b77e5a71a68b617c3347e90b
NoSpectators <john.c.erickson@gmail.com>
2026-06-20 12:49:45 -0400

    works!

---
+++ ch4/printRange.c
@@ -0,0 +1,36 @@
+/* Write a method called printRange that accepts two integers as arguments and 
+ prints the sequence of numbers between the two arguments, separated by 
+ spaces. Print an increasing sequence if the first argument is smaller than 
+ the second; otherwise, print a decreasing sequence. If the two numbers are 
+ the same, that number should be printed by itself.
+*/
+
+#include <stdio.h>
+
+
+void printRange(int a, int b);
+
+
+int main() {
+    int a, b;
+    printf("enter num1: ");
+    scanf("%d", &a); 
+    printf("enter num2: ");
+    scanf("%d", &b);
+    printRange(a, b);
+    return 0;
+}
+
+void printRange(int a, int b) {
+    int step = (a <= b) ? 1 : -1; // ternary 
+
+    for (int i = a; ; i+= step) { // infinite loop
+        printf("%d ", i);
+
+	if (i == b) { // manually stop infinite loop
+            break;
+	}
+    }
+    printf("\n");
+}
+
